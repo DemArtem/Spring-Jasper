335e71d8e44e209c2fb6c1d328ec79c0e16ffc29
gk <demar.demidov@yandex.ru>
2024-09-09 19:31:41 +0300

    test

---
+++ src/main/java/org/itstep/customer/Customer.java
@@ -0,0 +1,27 @@
+package org.itstep.customer;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+@Entity
+@Data
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String lastName;
+    private String firstName;
+    private String email;
+    private String company;
+    private String phone;
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+} 
---
+++ src/main/java/org/itstep/customer/CustomerController.java
@@ -0,0 +1,19 @@
+package org.itstep.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+    @Autowired
+    CustomerService customerService;
+
+    @GetMapping(value="/customers", produces="application/json")
+    public List<Customer> getCustomers(){
+        return customerService.findAll();
+    }
+
+
+} 
